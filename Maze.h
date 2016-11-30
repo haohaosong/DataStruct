eddfb402953544fa356e5bbf8d012ee9d672dc93
songjiahao <742398269@qq.com>
2016-11-30 09:56:51 +0800

    Maze

---
+++ Maze.h
@@ -0,0 +1,207 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+using namespace std;
+#include<stack>
+const int N = 10;
+
+struct Pos
+{
+	size_t _row;//行
+	size_t _col;//列
+};
+
+void PrintMaze(int *maze,size_t N)
+{
+	for (size_t i = 0; i < N; ++i)
+	{
+		for (size_t j = 0; j < N; ++j)
+		{
+			cout << maze[i*N + j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+bool CheckIsAccess(int *maze, size_t N, Pos next)
+{
+	if (maze[next._row*N + next._col] == 0)
+	{
+		return true;
+	}
+	return false;
+}
+
+void GetMaze(int *maze, size_t N)
+{
+	FILE *fp = fopen("1.txt","r");
+	if (fp == NULL)
+	{
+		throw invalid_argument("Read maze filed");
+	}
+	for (size_t i = 0; i < N; ++i)
+	{
+		for (size_t j = 0; j < N;)
+		{	
+			char ch = fgetc(fp);
+			if (ch == '0' || ch == '1')
+			{	
+				maze[i*N + j] = ch - '0';
+				++j;
+			}
+			else if (ch == EOF)
+			{
+				throw invalid_argument("Maze Error");
+			}
+		}
+	}
+}
+
+bool GetMazePath(int *maze, size_t n, Pos entry, stack<Pos>&path)
+{
+	maze[entry._row*N + entry._col] = 2;
+	Pos cur,next;
+	cur = entry;
+	path.push(entry);
+	while (!path.empty())
+	{
+		cur = path.top();
+
+		if (cur._row == N - 1)
+		{
+			return true;
+		}
+
+		next = cur;
+
+		next._row += 1;
+		if (CheckIsAccess((int*)maze, N, next) == true)
+		{
+			path.push(next);
+			maze[next._row*N + next._col] = 2;
+			continue;
+		}
+		next = cur;
+
+		next._row -= 1;
+		if (CheckIsAccess((int*)maze, N, next) == true)
+		{
+			path.push(next);
+			maze[next._row*N + next._col] = 2;
+			continue;
+		}
+		next = cur;
+
+		next._col += 1;
+		if (CheckIsAccess((int*)maze, N, next) == true)
+		{
+			path.push(next);
+			maze[next._row*N + next._col] = 2;
+			continue;
+		}
+		next = cur;
+
+		next._col -= 1;
+		if (CheckIsAccess((int*)maze, N, next) == true)
+		{
+			path.push(next);
+			maze[next._row*N + next._col] = 2;
+			continue;
+		}
+		next = cur;
+
+		path.pop();
+	}
+	return false;
+}
+
+void GetMazePath_R(int *maze, size_t N, Pos entry, stack<Pos>&path)
+{
+	if (entry._row == N - 1)
+	{
+		return;
+	}
+	Pos cur,next;
+	cur = entry;
+
+	next = cur;
+
+	next._row += 1;
+	if (CheckIsAccess((int*)maze,N,next) ==  true)
+	{
+		path.push(next);
+		maze[next._row*N + next._col] = 2;
+		GetMazePath_R(maze, N, next, path);
+	}
+	next = cur;
+
+	next._row -= 1;
+	if (CheckIsAccess((int*)maze, N, next) == true)
+	{
+		path.push(next);
+		maze[next._row*N + next._col] = 2;
+		GetMazePath_R(maze, N, next, path);
+	}
+	next = cur;
+
+	next._col += 1;
+	if (CheckIsAccess((int*)maze, N, next) == true)
+	{
+		path.push(next);
+		maze[next._row*N + next._col] = 2;
+		GetMazePath_R(maze, N, next, path);
+	}
+	next = cur;
+
+	next._col -= 1;
+	if (CheckIsAccess((int*)maze, N, next) == true)
+	{
+		path.push(next);
+		maze[next._row*N + next._col] = 2;
+		GetMazePath_R(maze, N, next, path);
+	}
+	next = cur;
+}
+
+void ReMaze(int* maze)
+{
+	for (size_t i = 0; i < N; ++i)
+	{
+		for (size_t j = 0; j < N; ++j)
+		{
+			if (maze[i*N + j] != 1)
+			{
+				maze[i*N + j] = 0;
+			}
+		}
+	}
+}
+
+void TestMaze()
+{
+	try
+	{
+		int maze[N][N] = { 0 };
+		int curmaze[N][N] = { 0 }; 
+		GetMaze((int*)maze, N);
+		stack<Pos> path,MinPath,empty;
+		
+		while (GetMazePath((int*)maze, N, { 2, 0 }, path))
+		{
+			if (path.size() < MinPath.size()||MinPath.size() == 0)
+			{
+				MinPath = path;
+			}
+			ReMaze((int*)maze);
+			maze[path.top()._row][path.top()._col] = 1;//将上次的出口改为1
+			path = empty;
+		}
+		cout << "Min path:" << MinPath.size() << endl;
+	}
+	catch (exception &e)
+	{
+		cout << e.what() << endl;
+	}
+}
+
