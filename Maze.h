00b6311bccc2a618bf9f9bbb4fd728b0701ea914
haohaosong <742398269@qq.com>
2017-03-23 15:08:05 +0800

    hhs:进行注释的优化

--- Maze.h
+++ Maze.h
@@ -1,8 +1,8 @@
 /*
 * author:haohaosong
 * date:2016/11/30
-* note:This head file need the file name with maze.txt.
-*   You can made a new file or cpoy the file form my repository
+* note:迷宫的实现，需要文件 maze.txt
+* 可以自己实现 一个 maze.txt 也可以直接用本库提供的maze.txt
 */
 
 #define _CRT_SECURE_NO_WARNINGS 1
@@ -18,12 +18,14 @@ struct Pos
 	size_t _col;//列
 };
 
+//打印当前的地图
 void PrintMaze(int *maze,size_t N)
 {
 	for (size_t i = 0; i < N; ++i)
 	{
 		for (size_t j = 0; j < N; ++j)
 		{
+			//通过偏移来求当前的元素
 			cout << maze[i*N + j] << " ";
 		}
 		cout << endl;
@@ -31,6 +33,7 @@ void PrintMaze(int *maze,size_t N)
 	cout << endl;
 }
 
+//判断一个点是否可以走
 bool CheckIsAccess(int *maze, size_t N, Pos next)
 {
 	if (maze[next._row*N + next._col] == 0)
@@ -42,6 +45,7 @@ bool CheckIsAccess(int *maze, size_t N, Pos next)
 
 bool CheckIsAccess(int *maze, size_t N, Pos next, Pos cur)
 {
+	//越界，无法走
 	if (next._row < 0 || next._row >= N || next._col < 0 || next._col >= N)
 	{
 		return false;
@@ -54,17 +58,23 @@ bool CheckIsAccess(int *maze, size_t N, Pos next, Pos cur)
 	return (maze[next._row*N + next._col] > (maze[cur._row*N + cur._col] + 1));
 }
 
+//打开maze.txt从而得到该地图
 void GetMaze(int *maze, size_t N)
 {
 	FILE *fp = fopen("1.txt","r");
 	if (fp == NULL)
 	{
+		//如果打开失败，抛出异常
 		throw invalid_argument("Read maze filed");
 	}
+	
+	//进行数组的初始化
 	for (size_t i = 0; i < N; ++i)
 	{
 		for (size_t j = 0; j < N;)
 		{	
+			//如果是0或1，初始化
+			//如果出现其他字符，说明地图有误抛出异常
 			char ch = fgetc(fp);
 			if (ch == '0' || ch == '1')
 			{	
@@ -79,6 +89,7 @@ void GetMaze(int *maze, size_t N)
 	}
 }
 
+//得到迷宫的路径
 bool GetMazePath(int *maze, size_t n, Pos entry, stack<Pos>&path)
 {
 	maze[entry._row*N + entry._col] = 2;
@@ -89,13 +100,15 @@ bool GetMazePath(int *maze, size_t n, Pos entry, stack<Pos>&path)
 	{
 		cur = path.top();
 
+		//如果到达边界，则找到了出口
 		if (cur._row == N - 1)
 		{
 			return true;
 		}
 
 		next = cur;
-
+		
+		//向下走
 		next._row += 1;
 		if (CheckIsAccess((int*)maze, N, next) == true)
 		{
@@ -104,7 +117,8 @@ bool GetMazePath(int *maze, size_t n, Pos entry, stack<Pos>&path)
 			continue;
 		}
 		next = cur;
-
+	
+		//向上走
 		next._row -= 1;
 		if (CheckIsAccess((int*)maze, N, next) == true)
 		{
@@ -114,6 +128,7 @@ bool GetMazePath(int *maze, size_t n, Pos entry, stack<Pos>&path)
 		}
 		next = cur;
 
+		//向右走
 		next._col += 1;
 		if (CheckIsAccess((int*)maze, N, next) == true)
 		{
@@ -123,6 +138,7 @@ bool GetMazePath(int *maze, size_t n, Pos entry, stack<Pos>&path)
 		}
 		next = cur;
 
+		//向左走
 		next._col -= 1;
 		if (CheckIsAccess((int*)maze, N, next) == true)
 		{
@@ -131,12 +147,15 @@ bool GetMazePath(int *maze, size_t n, Pos entry, stack<Pos>&path)
 			continue;
 		}
 		next = cur;
-
+	
+		//如果上述都无法走，则返回上一个点
+		//方法是弹出栈顶元素
 		path.pop();
 	}
 	return false;
 }
 
+//递归得到迷宫的路径
 void GetMazePath_R(int *maze, size_t N, Pos entry, stack<Pos>&path)
 {
 	if (entry._row == N - 1)
@@ -184,6 +203,8 @@ void GetMazePath_R(int *maze, size_t N, Pos entry, stack<Pos>&path)
 		next = cur;
 	}
 }
+
+//递归去找最佳的路径
 void GetMazePath_R(int *maze, size_t N, Pos entry, stack<Pos>&path, stack<Pos>&shortpath)
 {
 	if (entry._row == N - 1)
@@ -235,6 +256,8 @@ void GetMazePath_R(int *maze, size_t N, Pos entry, stack<Pos>&path, stack<Pos>&s
 	}
 	path.pop();
 }
+
+//进行地图的重置
 void ReMaze(int* maze)
 {
 	for (size_t i = 0; i < N; ++i)
@@ -250,12 +273,11 @@ void ReMaze(int* maze)
 }
 
 /*
-* note:this method has the bug when the rode has a circle
-*		however,the method while name is 
-*		"void GetMazePath_R(int *maze, size_t N, Pos entry, stack<Pos>&path, stack<Pos>&shortpath)" 
-*		can slove this problem
+* note:这个方法有个小问题，挡出现环的时候无法处理
+*   但是 void GetMazePath_R(int *maze, size_t N, Pos entry, stack<Pos>&path, stack<Pos>&shortpath) 可以处理这个问题
 */
 
+//求最小路径
 stack<Pos> MinPath()
 {
 	int maze[N][N] = { 0 };
@@ -269,17 +291,21 @@ stack<Pos> MinPath()
 		{
 			MinPath = path;
 		}
+		
+		//重置地图，求下一个解
 		ReMaze((int*)maze);
-		maze[path.top()._row][path.top()._col] = 1;//将上次的出口改为1
+		
+		//将上次的出口改为1
+		maze[path.top()._row][path.top()._col] = 1;
+		
 		path = empty;
 	}
 	return MinPath;
 }
-/*
-* note:the example of how to use GetMazePath_R to seek the short path.
-*/
+
 void TestMaze()
 {
+	//捕获异常，进行异常的处理
 	try
 	{
 		int maze[N][N] = { 0 };
